121a90c45da4f584abb5e0e57dfdbf1515395ded
Dominic Faulring <domcodesjs@gmail.com>
2021-01-17 17:12:07 -0800

    added case study

--- javascript/main.js
+++ javascript/main.js
@@ -6,60 +6,80 @@ document.addEventListener('DOMContentLoaded', function () {
   const email = document.getElementById('email');
   const message = document.getElementById('message');
   const submitBtn = document.getElementById('contact-form-submit');
+  const invisionButton = document.querySelector('.invision-btn');
+  const invisionButtonEnd = document.querySelector('.invision-btn-end button');
 
-  form.addEventListener('submit', function (e) {
-    e.preventDefault();
+  if (invisionButton) {
+    invisionButton.addEventListener('click', () =>
+      window.open(
+        'https://invis.io/Z5HOJMHTHSG#/276496524_Landing-View-Desktop'
+      )
+    );
 
-    if (
-      name.value.trim() === '' ||
-      email.value.trim() === '' ||
-      message.value.trim() === ''
-    ) {
-      return;
-    }
+    invisionButtonEnd.addEventListener('click', () =>
+      window.open(
+        'https://invis.io/Z5HOJMHTHSG#/276496524_Landing-View-Desktop'
+      )
+    );
+  }
 
-    submitBtn.disabled = true;
-    name.disabled = true;
-    email.disabled = true;
-    message.disabled = true;
+  if (form) {
+    form.addEventListener('submit', function (e) {
+      e.preventDefault();
 
-    fetch('https://formspree.io/moqkgpoz', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        name: name.value,
-        email: email.value,
-        message: message.value,
-      }),
-    })
-      .then((res) => {
-        if (res.status !== 200) {
+      if (
+        name.value.trim() === '' ||
+        email.value.trim() === '' ||
+        message.value.trim() === ''
+      ) {
+        return;
+      }
+
+      submitBtn.disabled = true;
+      name.disabled = true;
+      email.disabled = true;
+      message.disabled = true;
+
+      fetch('https://formspree.io/moqkgpoz', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: name.value,
+          email: email.value,
+          message: message.value
+        })
+      })
+        .then((res) => {
+          if (res.status !== 200) {
+            name.removeAttribute('disabled');
+            email.removeAttribute('disabled');
+            message.removeAttribute('disabled');
+            submitBtn.removeAttribute('disabled');
+            return alert(
+              'Something went wrong. Please send your message again.'
+            );
+          }
+
+          name.value = '';
+          email.value = '';
+          message.value = '';
+          name.removeAttribute('disabled');
+          email.removeAttribute('disabled');
+          message.removeAttribute('disabled');
+          submitBtn.removeAttribute('disabled');
+          return alert('Messsage sent!');
+        })
+        .catch((err) => {
+          console.log(err);
           name.removeAttribute('disabled');
           email.removeAttribute('disabled');
           message.removeAttribute('disabled');
           submitBtn.removeAttribute('disabled');
           return alert('Something went wrong. Please send your message again.');
-        }
-
-        name.value = '';
-        email.value = '';
-        message.value = '';
-        name.removeAttribute('disabled');
-        email.removeAttribute('disabled');
-        message.removeAttribute('disabled');
-        submitBtn.removeAttribute('disabled');
-        return alert('Messsage sent!');
-      })
-      .catch((err) => {
-        console.log(err);
-        name.removeAttribute('disabled');
-        email.removeAttribute('disabled');
-        message.removeAttribute('disabled');
-        submitBtn.removeAttribute('disabled');
-        return alert('Something went wrong. Please send your message again.');
-      });
-  });
+        });
+    });
+  }
 });
